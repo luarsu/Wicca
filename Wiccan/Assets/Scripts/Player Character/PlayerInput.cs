ec7e13edbc46ac79170b513aac7d708ade30bb58
Luis Armas <luarsu@gmail.com>
2020-02-23 16:48:53 +0000

    Added player input basics

--- Wiccan/Assets/Scripts/Player Character/PlayerInput.cs
+++ Wiccan/Assets/Scripts/Player Character/PlayerInput.cs
@@ -1,18 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
+using System.Collections;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PlayerInput Instance
     {
-        
+        get { return s_Instance; }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Singleton class for the player input
+    protected static PlayerInput s_Instance;
+
+    [HideInInspector]
+    public bool playerControllerInputBlocked;
+
+    protected Vector2 m_Movement;
+    protected Vector2 m_Camera;
+    protected bool m_Jump;
+    protected bool m_Attack;
+    protected bool m_Pause;
+    protected bool m_ExternalInputBlocked;
+
+
+
+    //The following methods just return a value calculated during update.
+    //Either a bool or a vector2
+
+    //If the player movement is blocked then return vector2.zero. 
+    //Else return the movement calculated during update
+    public Vector2 MoveInput
     {
-        
+        get
+        {
+            if (playerControllerInputBlocked || m_ExternalInputBlocked)
+                return Vector2.zero;
+            return m_Movement;
+        }
+    }
+
+    public Vector2 CameraInput
+    {
+        get
+        {
+            if (playerControllerInputBlocked || m_ExternalInputBlocked)
+                return Vector2.zero;
+            return m_Camera;
+        }
+    }
+
+    public bool JumpInput
+    {
+        get { return m_Jump && !playerControllerInputBlocked && !m_ExternalInputBlocked; }
+    }
+
+    public bool Attack
+    {
+        get { return m_Attack && !playerControllerInputBlocked && !m_ExternalInputBlocked; }
+    }
+
+    public bool Pause
+    {
+        get { return m_Pause; }
     }
 }
