9ca11f2ba9a0c187074f9f8f471267d85cdedaa0
Luis Armas <luarsu@gmail.com>
2020-03-02 21:44:18 +0000

    Started testing with the dialogue bubbles that come out directly from the character

---
+++ Wiccan/Assets/Scripts/DialogueSystem/TestBubbleSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBubbleSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform target1;
+    public Transform target2;
+    public float xOffset = 50f;
+    public float yOffset = 85f;
+    bool whichTarget;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1"))
+        {
+            whichTarget = true;
+        }
+        if(Input.GetKeyDown("2"))
+        {
+            whichTarget = false;
+        }
+
+        Vector2 BubblePosition;
+        if (whichTarget)
+        {
+            BubblePosition = RectTransformUtility.WorldToScreenPoint(Camera.main, target1.transform.position);
+        }
+        else
+        {
+            BubblePosition = RectTransformUtility.WorldToScreenPoint(Camera.main, target2.transform.position);
+        }
+        BubblePosition.y += yOffset;
+        BubblePosition.x += xOffset;
+        transform.position = BubblePosition;
+    }
+}
