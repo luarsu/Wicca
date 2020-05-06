0148f8fc00cb8138f96bcae07f458dcf4343f1e3
Luis Armas <luarsu@gmail.com>
2020-05-06 21:18:55 +0100

    Added simple bubble system for the dialogue

--- Wiccan/Assets/Scripts/DialogueSystem/TestBubbleSystem.cs
+++ Wiccan/Assets/Scripts/DialogueSystem/TestBubbleSystem.cs
@@ -8,13 +8,17 @@ public class TestBubbleSystem : MonoBehaviour
 
     public Transform target1;
     public Transform target2;
-    public float xOffset = 50f;
-    public float yOffset = 85f;
-    bool whichTarget;
+    public Transform currentTarget;
+    public RectTransform BubbleTransform;
+    public RectTransform TriangleTransform;
+    public float yOffset = 15f;
+    public float maxDistanceBubbleToTriangle = 90;
+
+    private float offsetBubbleToTriangle = 72;
     
     void Start()
     {
-        
+        currentTarget = target1;
     }
 
     // Update is called once per frame
@@ -22,24 +26,58 @@ public class TestBubbleSystem : MonoBehaviour
     {
         if(Input.GetKeyDown("1"))
         {
-            whichTarget = true;
+            currentTarget = target1;
+            setBubble();
         }
         if(Input.GetKeyDown("2"))
         {
-            whichTarget = false;
+            currentTarget = target2;
+            setBubble();
         }
 
-        Vector2 BubblePosition;
-        if (whichTarget)
-        {
-            BubblePosition = RectTransformUtility.WorldToScreenPoint(Camera.main, target1.transform.position);
-        }
-        else
-        {
-            BubblePosition = RectTransformUtility.WorldToScreenPoint(Camera.main, target2.transform.position);
-        }
+        updateBubblePosition();
+
+        
+    }
+
+    public void setBubble()
+    {
+        Vector2 BubblePosition = RectTransformUtility.WorldToScreenPoint(Camera.main, currentTarget.transform.position);
+
+        //Add the offset from the target object to look good and set the position for the triangle
         BubblePosition.y += yOffset;
-        BubblePosition.x += xOffset;
-        transform.position = BubblePosition;
+        TriangleTransform.position = BubblePosition;
+        //Add the offset from the triangle to the bubble to look right and set the position of the bubble
+        BubblePosition.y += offsetBubbleToTriangle;
+        BubbleTransform.position = BubblePosition;
+    }
+
+    public void updateBubblePosition()
+    {
+        //here we just update the x position of the triangle, and we update the position of the bubble when is further than it should
+        Vector2 TrianglePosition = RectTransformUtility.WorldToScreenPoint(Camera.main, currentTarget.transform.position);
+
+        //update the triangle positiom
+        TrianglePosition.y += yOffset;
+        TriangleTransform.position = TrianglePosition;
+
+        //the bubble position only updates the Y unless it's too far from the triangle in the x axis
+        Vector2 BubblePosition = new Vector2(BubbleTransform.position.x, TrianglePosition.y + offsetBubbleToTriangle);
+        float distanceTriangleToBubble = Mathf.Abs(TrianglePosition.x - BubblePosition.x);
+
+        //If the bubble is too far from the triangle, update the x of the bubble position
+        if (distanceTriangleToBubble  > maxDistanceBubbleToTriangle)
+        {
+            if(BubblePosition.x > TrianglePosition.x)
+            {
+                BubblePosition.x -= Mathf.Abs(distanceTriangleToBubble - maxDistanceBubbleToTriangle);
+            }
+            else
+            {
+                BubblePosition.x += Mathf.Abs(distanceTriangleToBubble - maxDistanceBubbleToTriangle);
+            }
+        }
+        //update the bubble position
+        BubbleTransform.position = BubblePosition;
     }
 }
