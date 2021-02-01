f2f387d5f48e9873b4c27cf98ee04ac45574ed7e
luarsu <luarsu@gmail.com>
2021-02-01 16:40:53 +0100

    Added acceleration/deceleration to demon movement based on how close/far it is from the target

--- Wiccan/Assets/Scripts/Demon/DemonMovement.cs
+++ Wiccan/Assets/Scripts/Demon/DemonMovement.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class DemonMovement : MonoBehaviour
 {
     public Transform Followtarget;
-    public float Velocity;
-    public float FollowOffset;
+    public float minVelocity;
+    public float maxVelocity;
+    public float minFollowDistance;
+    public float maxFollowDistance;
     public float desiredRotationSpeed = 0.1f;
 
     protected CharacterController characterController;      // Reference used to actually move the character.
+    protected float distance;
     protected Vector3 desiredMoveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(Followtarget.position, gameObject.transform.position);
+        distance = Vector3.Distance(Followtarget.position, gameObject.transform.position);
 
         //if the demon is further away than the offset distance then start moving
-        if (Mathf.Abs(dist) > FollowOffset)
+        if (Mathf.Abs(distance) > minFollowDistance)
         {
             FollowTarget();
         }
@@ -28,7 +31,12 @@ public class DemonMovement : MonoBehaviour
     {
         desiredMoveDirection = Followtarget.position - gameObject.transform.position;
 
+
+        //adjust velocity based on how close or far it is from the target
+        float lerpValue = distance / maxFollowDistance;
+        float adjustedVelocity = Mathf.Lerp(minVelocity, maxVelocity, lerpValue);
+
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
-        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Followtarget.position, Time.deltaTime * Velocity);
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Followtarget.position, Time.deltaTime * adjustedVelocity);
     }
 }
